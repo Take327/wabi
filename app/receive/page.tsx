fd507f763e7fc33ab32516ed4e650eae7e3ae235
Take327 <gatewayseed2@gmail.com>
2024-12-15 20:55:00 +0900

    お焚き上げアニメーション実施

--- app/receive/page.tsx
+++ app/receive/page.tsx
@@ -30,33 +30,39 @@ export default function ReceivePage() {
   };
 
   return (
-    <div className="flex flex-col items-center min-h-screen px-4 py-8 bg-gray-100">
+    <div className="flex flex-col items-center min-h-screen px-4 py-8 bg-gray-100 relative">
       <h2 className="text-lg font-semibold text-gray-700 mb-2">WABIについて</h2>
       <p className="text-gray-600">
         WABIは、人前では伝えきれない思いを静かに伝えるためのサービスです。
         <br />
         謝罪、別れの言葉、お悔やみのメッセージなど、大切な気持ちを言葉にして相手に届けることができます。
       </p>
-      
-      {isBurning && <div className="flex min-w-full items-center  max-w-5xl"><FireAnimation /></div>} {/* FireAnimation を表示 */}
-      {message && messageType ? (
-        <>
-          <MessageCard messageType={messageType} message={message} />
-          <p className="text-gray-600">
-            メッセージを受け入れられない場合は燃やしてしまいましょう<br/>
-            ※送信者への通知などはありません。送信者に意図が伝わらないようにデータの削除も行われません。<br/>
-            あなたの中でこのアドレスを削除して開かないようにしましょう。
-          </p>
-          <button
-            onClick={handleBurn}
-            className="mt-4 px-6 py-3 bg-red-500 text-white rounded shadow hover:bg-red-600 transition"
-          >
-            燃やす
-          </button>
-        </>
-      ) : (
-        <p className="text-gray-600">メッセージが見つかりません。</p>
-      )}
+
+      <div className="relative w-full max-w-5xl">
+        {isBurning && (
+          <div className="absolute flex justify-center inset-0 z-10">
+            <FireAnimation />
+          </div>
+        )}
+        {message && messageType ? (
+          <>
+            <MessageCard messageType={messageType} message={message} />
+            <p className="text-gray-600 mt-4">
+              メッセージを受け入れられない場合は"お焚き上げ"しましょう<br />
+              ※送信者への通知などはありません。送信者に意図が伝わらないようにデータの削除も行われません。<br />
+              あなたの中でこのアドレスを削除して開かないようにしましょう。
+            </p>
+            <button
+              onClick={handleBurn}
+              className="mt-4 px-6 py-3 bg-red-500 text-white rounded shadow hover:bg-red-600 transition"
+            >
+              お焚き上げ
+            </button>
+          </>
+        ) : (
+          <p className="text-gray-600">メッセージが見つかりません。</p>
+        )}
+      </div>
     </div>
   );
 }
